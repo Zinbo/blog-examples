7d5128fa88c1e419b2646abac0a153ccdb863f06
Zinbo <superzinbo@gmail.com>
2023-05-09 21:01:36 +0100

    Add code for Spring Boot Correlation Ids blog post.

---
+++ correlation-ids-spring-boot-3/src/main/java/com/stacktobasics/correlationidsspringboot3/api/HelloWorldController.java
@@ -0,0 +1,31 @@
+package com.stacktobasics.correlationidsspringboot3.api;
+
+import com.stacktobasics.correlationidsspringboot3.service.HelloWorldService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping
+@Slf4j
+public class HelloWorldController {
+
+    private final HelloWorldService helloWorldService;
+
+    public HelloWorldController(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> sayHello() {
+        log.info("Someone called the /hello endpoint");
+        return ResponseEntity.ok(helloWorldService.sayHello());
+    }
+
+    @GetMapping("/bad-call")
+    public void badCall() {
+        log.info("Someone called the /bad-call endpoint");
+        helloWorldService.fakeBadCall();
+    }
+}
